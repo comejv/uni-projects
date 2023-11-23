9a163bc2942dfa96db8b49cab881593decee3017
Axel Deleuze-Dordron <75162059+Stonksmen@users.noreply.github.com>
2023-11-23 09:49:54 +0100

    TODO 1 fait

--- CEBD/actions/F1.py
+++ CEBD/actions/F1.py
@@ -8,7 +8,6 @@ class Window(tk.Toplevel):
         # Définition de la taille de la fenêtre, du titre et des lignes/colonnes de l'affichage grid
         display.centerWindow(600, 400, self)
         self.title('F1 : températures moyennes des départements au 2018-01-01')
-        #TODO Q1 Aller voir le code de defineGridDisplay dans utils/display.py
         display.defineGridDisplay(self, 1, 1)
 
         # On définit les colonnes que l'on souhaite afficher dans la fenêtre et la requête
@@ -19,6 +18,5 @@ class Window(tk.Toplevel):
                     ORDER BY code_departement"""
 
         # On utilise la fonction createTreeViewDisplayQuery pour afficher les résultats de la requête
-        #TODO Q1 Aller voir le code de createTreeViewDisplayQuery dans utils/display.py
         tree = display.createTreeViewDisplayQuery(self, columns, query,200)
         tree.grid(row=0, sticky="nswe")

cdf1d076c5da02aaa65d9d5b996854d53626c130
Côme VINCENT <44554692+comejv@users.noreply.github.com>
2023-12-03 18:08:30 +0100

    show mean temp as graph
    Signed-off-by: Côme VINCENT <44554692+comejv@users.noreply.github.com>

--- CEBD/actions/Q6.py
+++ CEBD/actions/Q6.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import matplotlib.pyplot as mp
+import matplotlib.pyplot as plt
 import numpy as np
 from utils import display
 from utils import db
@@ -26,44 +26,31 @@ class Window(tk.Toplevel):
             font=("Helvetica", "10", "bold"),
         ).grid(sticky="we", row=0)
 
-        ttk.Button(self, text="Afficher le graph", command=self.printgraph).grid(column=0, row=1)
+        ttk.Button(self, text="Afficher le graph", command=self.printgraph).grid(
+            column=0, row=1
+        )
 
     def printgraph(self):
-
         try:
             tab = []
             query = """
-                    SELECT strftime('%MM', date_mesure) as mois_mesure,
-                           MIN(temperature_min_mesure) as minimum
+                    SELECT strftime('%m', date_mesure) AS month, AVG(temperature_min_mesure) AS avg_temperature
                     FROM Mesures
-                    WHERE code_departement = 38 AND strftime('%YYYY', date_mesure) = 2022
-                    GROUP BY strftime('%MM', date_mesure)
+                    WHERE code_departement = '38' AND strftime('%Y', date_mesure) = '2022'
+                    GROUP BY month;
                 """
             cursor = db.data.cursor()
-            result = cursor.execute(query)
+            result = cursor.execute(query).fetchall()
+            months, temperatures = zip(*result)
 
-            for res in result:
-                tab.append(
-                    [
-                        res[0],
-                        res[1],
-                    ]
-                )
         except Exception as e:
             print("Erreur : " + repr(e))
+            return
 
-        months = 5
-
-        # make data
-        x = months
-        y = np.linspace(-5, 10, 12)
-
-        # plot
-        fig, ax = mp.subplots()
-
-        ax.plot(x, y, linewidth=2.0)
-
-        ax.set(xlim=(0, 11), xticks=np.arange(1, 12),
-               ylim=(-15, 30), yticks=np.arange(-15, 30))
-
-        mp.show()
+        # Plot the data
+        plt.plot(months, temperatures, label="Average Temperature")
+        plt.xlabel("Month")
+        plt.ylabel("Value")
+        plt.title("Correlation of temperatures and costs in Isère / 2022")
+        plt.legend()
+        plt.show()

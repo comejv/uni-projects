93422293e9853796ef7bb8aa54c39e76d3c58084
Côme VINCENT <44554692+comejv@users.noreply.github.com>
2023-12-03 17:49:21 +0100

    delete useless Q5 code
    Signed-off-by: Côme VINCENT <44554692+comejv@users.noreply.github.com>

--- CEBD/actions/Q5.py
+++ CEBD/actions/Q5.py
@@ -83,52 +83,7 @@ class Window(tk.Toplevel):
 
         except Exception as e:
             print("Erreur : " + repr(e))
-            # Pour chaque département...
-            for dept in result:
-                # On récupère les années où il y a des mesures pour ce département
-                try:
-                    cursor2 = db.data.cursor()
-                    result2 = cursor2.execute(
-                        """
-                        SELECT DISTINCT strftime('%Y', date_mesure) as annee
-                        FROM Mesures
-                        WHERE Mesures.code_departement = ?
-                    """,
-                        [dept[0]],
-                    )
-                    nbquery += 1
-                except Exception as e:
-                    print("Erreur : " + repr(e))
-                else:
-                    # Pour chaque année...
-                    for annee in result2:
-                        # On récupère les mesures pour ce département et cette année
-                        try:
-                            cursor3 = db.data.cursor()
-                            result3 = cursor3.execute(
-                                """
-                                SELECT ROUND(avg(temperature_moy_mesure), 2) AS moyenne, min(temperature_min_mesure) AS minimum, max(temperature_max_mesure) AS maximum
-                            FROM Mesures
-                            WHERE Mesures.code_departement = ? AND strftime('%Y', date_mesure) = ?
-                            """,
-                                [dept[0], annee[0]],
-                            )
-                            nbquery += 1
-                        except Exception as e:
-                            print("Erreur : " + repr(e))
-                        else:
-                            # On ajoute notre mesure dans le tableau
-                            for mesures in result3:
-                                tab.append(
-                                    [
-                                        dept[0],
-                                        dept[1],
-                                        annee[0],
-                                        mesures[0],
-                                        mesures[1],
-                                        mesures[2],
-                                    ]
-                                )
+            return
 
         # On affiche les données du tableau dans la treeview
         for row in tab:

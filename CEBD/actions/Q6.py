372a256f19c7ba8401c622c12863e29799873730
Axel Deleuze-Dordron <75162059+Stonksmen@users.noreply.github.com>
2023-12-03 17:18:03 +0100

    Beginning Q6

--- CEBD/actions/Q6.py
+++ CEBD/actions/Q6.py
@@ -1,5 +1,8 @@
 import tkinter as tk
+import matplotlib.pyplot as mp
+import numpy as np
 from utils import display
+from utils import db
 from tkinter import ttk
 
 
@@ -22,3 +25,45 @@ class Window(tk.Toplevel):
             anchor="center",
             font=("Helvetica", "10", "bold"),
         ).grid(sticky="we", row=0)
+
+        ttk.Button(self, text="Afficher le graph", command=self.printgraph).grid(column=0, row=1)
+
+    def printgraph(self):
+
+        try:
+            tab = []
+            query = """
+                    SELECT strftime('%MM', date_mesure) as mois_mesure,
+                           MIN(temperature_min_mesure) as minimum
+                    FROM Mesures
+                    WHERE code_departement = 38 AND strftime('%YYYY', date_mesure) = 2022
+                    GROUP BY strftime('%MM', date_mesure)
+                """
+            cursor = db.data.cursor()
+            result = cursor.execute(query)
+
+            for res in result:
+                tab.append(
+                    [
+                        res[0],
+                        res[1],
+                    ]
+                )
+        except Exception as e:
+            print("Erreur : " + repr(e))
+
+        months = 5
+
+        # make data
+        x = months
+        y = np.linspace(-5, 10, 12)
+
+        # plot
+        fig, ax = mp.subplots()
+
+        ax.plot(x, y, linewidth=2.0)
+
+        ax.set(xlim=(0, 11), xticks=np.arange(1, 12),
+               ylim=(-15, 30), yticks=np.arange(-15, 30))
+
+        mp.show()
